78ad054155c35edfca80b52c14fa4c62524263f9
davimdantas <davimdantas@gmail.com>
2019-12-31 15:40:22 -0300

    Enviromment setup

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "postgres",
+    host: "tuffi.db.elephantsql.com",
+    username: "fycinhgj",
+    password: "Q3cv8xBzTmLJh4XcWeF5k7H70_mKT12G",
+    database: "fycinhgj",
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+};
